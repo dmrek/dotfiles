1c40881abfa071f9bedcc5321132544bb855d9cb
Dmitry Krek <dmitry.krek@gmail.com>
2026-02-12 18:36:08 +0100

    Migrate my vim config to nvim

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-h>", "40h")
+vim.keymap.set("n", "<C-l>", "40l")
+
+vim.keymap.set("n", "<Up>", "<C-u>")
+vim.keymap.set("n", "<Down>", "<C-d>")
+vim.keymap.set("n", "<Left>", "<C-h>")
+vim.keymap.set("n", "<Right>", "<C-l>")
+
+vim.keymap.set("n", "go", "o<Esc>")
+vim.keymap.set("n", "gO", "O<Esc>")
+vim.keymap.set("n", "U", "<C-r>")
+vim.keymap.set("n", "uh", "<cmd>nohlsearch<CR>")
