217e706084417ad565b7dfa3a0ff3e807cbce2e0
Dmitry Krek <dmitry.krek@gmail.com>
2026-03-05 15:57:12 +0100

    Add markdown-preview to nvim

---
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,15 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  build = "cd app && npm install",
+  ft = "markdown",
+  config = function()
+    if vim.fn.has("wsl") == 1 then
+      vim.g.mkdp_browserfunc = "OpenBrowser"
+      vim.cmd([[
+        function! OpenBrowser(url)
+          silent! execute '!cmd.exe /c start "" ' . a:url
+        endfunction
+      ]])
+    end
+  end,
+}
